a21b7666faacda0c6275d787b4bac8d31c6e9e5a
Rohied Abdoella <74419576+RohiedAbdoella@users.noreply.github.com>
2021-02-08 20:46:13 +0100

    Add files via upload

---
+++ bigger.js
@@ -0,0 +1,42 @@
+//checking is a number is big
+/*
+const checkNumber = function(number) {
+    if (number > 100) {
+    return "true";
+    }
+    return "false";
+};
+
+const result = checkNumber(101);
+console.log(result);
+*/
+
+//Bouncer at a club, Function that does something
+/*
+const BrendaBot = function(crowd, age) {
+    const crowdMax = 50,
+    const ageMinimum = 18,
+
+    if (crowdMax > 50, ageMinimum > 18) {
+        return "It's too busy now, come back later"
+    } else if(crowdMax < 50, ageMinimum > 18) {
+        return "come in"
+    }
+    
+    if (crowdMax > 50, ageMinimum < 18) {
+    return "this is a club for adults"
+    }
+};
+
+const result = BrendaBot(51, 18)
+console.log(result);
+*/
+
+//Calculating the average
+const average = function (number1, number2, number3, number4, number5) {
+    const outcome = math.round(number1, number2, number3, number4, number5);
+    return outcome;
+}
+
+const result = average(10, 20, 30, 40, 50)
+console.log(result);
